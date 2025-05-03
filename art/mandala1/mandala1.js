6925e8362d6b44a1d4b6bf9f7050d813430869f7
GabrielS <gabriel.somogyi.feuga@gmail.com>
2025-05-03 20:19:01 +0200

    adding url param for mandala

--- art/mandala1/mandala1.js
+++ art/mandala1/mandala1.js
@@ -1,10 +1,4 @@
-import {
-  drawLine,
-  drawCircle,
-  setupCanvas,
-  pointsOnCircle,
-  shuffleArray,
-} from "../shared/utils.js";
+import { drawLine, setupCanvas, pointsOnCircle } from "../shared/utils.js";
 import {
   downloadCanvasImage,
   setContentInfo,
@@ -19,19 +13,48 @@ const canvasctx = canvas.getContext("2d");
 setupCanvas(container, canvas, canvasctx);
 
 canvasctx.strokeStyle = "white";
-canvasctx.lineWidth = 0.35;
+canvasctx.lineWidth = 0.5;
 canvasctx.fillStyle = "black";
 canvasctx.fillRect(0, 0, canvas.width, canvas.height);
 
+const urlParams = new URLSearchParams(window.location.search);
+
 //params
+let innerFirst = urlParams.get("innerFirst") === "true" || false;
+let hue = 30; // Start with orange (hue 30)
+const hueIncrement = 0.5; // Adjust this for speed of color change
+const NB_POINTS_PER_CIRLCE = 20;
+const DELAY_BETWEEN_LINES_MS = 35; // Adjust for speed of animation (lower = faster)
 const middleX = canvas.width / 2;
 const middleY = canvas.height / 2;
 const heightWidthMax = Math.max(canvas.height, canvas.width);
+const heightWidthMin = Math.min(canvas.height, canvas.width);
+
+// outer circle
+const outerCenterX = middleX;
+const outerCenterY = middleY;
+const outerRadius = heightWidthMin / 2 - 10;
+const outerPoints = pointsOnCircle(
+  { x: outerCenterX, y: outerCenterY, radius: outerRadius },
+  NB_POINTS_PER_CIRLCE
+);
+
+// inner circle
+const innerCenterX = middleX;
+const innerCenterY = middleY;
+const innerRadius = (0.5 * Math.random() * heightWidthMin) / 2 - 10;
+const innerPoints = pointsOnCircle(
+  { x: innerCenterX, y: innerCenterY, radius: innerRadius },
+  NB_POINTS_PER_CIRLCE
+);
 
 // animate
 let animationFrameId = null;
 let currentIndex = 0;
 let lastTimestamp = 0;
+let nb_steps = NB_POINTS_PER_CIRLCE * NB_POINTS_PER_CIRLCE;
+let i = 0;
+let j = 0;
 function drawNext(timestamp) {
   // Initialize lastTimestamp if first run
   if (!lastTimestamp) {
@@ -42,34 +65,65 @@ function drawNext(timestamp) {
 
   // Only draw if enough time has passed
   if (elapsed >= DELAY_BETWEEN_LINES_MS) {
-    if (currentIndex >= NB_POINTS_PER_CIRLCE * circles.length) {
+    if (currentIndex >= nb_steps) {
       stopAnimation();
       console.log("Animation complete!");
       return;
     }
+    currentIndex++;
+    lastTimestamp = timestamp;
+    // draw next line
+    if (hue < 30) hue = 30; // Keep within orange-blue range
+    canvasctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    if (innerFirst) {
+      drawLine(innerPoints[i], outerPoints[j], canvasctx);
+    } else {
+      // Update color (hue goes from 30 to 240 - orange to blue)
+      hue = (hue + hueIncrement) % 240;
+      drawLine(outerPoints[i], innerPoints[j], canvasctx);
+    }
+    //update index
+    j++;
+    if (innerFirst) {
+      hue = hue + j * hueIncrement * 2;
+    }
+    if (j >= innerPoints.length) {
+      if (innerFirst) {
+        hue = 30;
+      }
+      j = 0;
+      i++;
+      if (i >= outerPoints.length) {
+        i = 0;
+      }
+    }
   }
-  currentIndex++;
-  lastTimestamp = timestamp;
   animationFrameId = requestAnimationFrame(drawNext);
 }
 
-function startAnimation() {}
+const startAnimation = () => {
+  if (!animationFrameId) {
+    animationFrameId = requestAnimationFrame(drawNext);
+  }
+};
 
-function stopAnimation() {
+const stopAnimation = () => {
   if (animationFrameId) {
     cancelAnimationFrame(animationFrameId);
     animationFrameId = null;
   }
-}
+};
 
-function resetAnimation() {
+const resetAnimation = () => {
   stopAnimation();
   // Clear canvas and reset variables
   canvasctx.clearRect(0, 0, canvas.width, canvas.height);
   canvasctx.fillRect(0, 0, canvas.width, canvas.height);
   currentIndex = 0;
   lastTimestamp = 0;
-}
+  i = 0;
+  j = 0;
+};
 
 const saveCanvasImage = () => {
   return downloadCanvasImage(canvas, TITLE);
--- art/particles/particles.js
+++ art/particles/particles.js
@@ -11,9 +11,9 @@ import {
   setMenuCallbacks,
 } from "../shared/controls.js";
 
-const TITLE = "Circles of Lines";
+const TITLE = "Particles";
 
-const canvas = document.getElementById("lines-canvas");
+const canvas = document.getElementById("particles-canvas");
 const linesContainer = document.getElementById("canvas-container");
 const canvasctx = canvas.getContext("2d");
 setupCanvas(linesContainer, canvas, canvasctx);
@@ -28,49 +28,16 @@ const middleX = canvas.width / 2;
 const middleY = canvas.height / 2;
 const heightWidthMax = Math.max(canvas.height, canvas.width);
 
-const NB_CIRCLE_PER_QUARTER = 8;
-const NB_POINTS_PER_CIRLCE = 360;
 const DELAY_BETWEEN_LINES_MS = 35; // Adjust for speed of animation (lower = faster)
 
 // params
-let showCircle = false;
 
-let circles = [];
-//top left
-for (let i = 0; i < NB_CIRCLE_PER_QUARTER; i++) {
-  const centerX = Math.floor(Math.random() * middleX);
-  const centerY = Math.floor(Math.random() * middleY);
-  const radius = Math.max(
-    heightWidthMax / 30,
-    Math.floor((Math.random() * heightWidthMax) / 2)
-  );
-  circles.push({ x: centerX, y: centerY, radius });
-}
-
-//bottom right
-for (let i = 0; i < NB_CIRCLE_PER_QUARTER; i++) {
-  const centerX = middleX + Math.floor(Math.random() * middleX);
-  const centerY = middleY + Math.floor(Math.random() * middleY);
-  const radius = Math.max(
-    heightWidthMax / 30,
-    Math.floor((Math.random() * heightWidthMax) / 2)
-  );
-  circles.push({ x: centerX, y: centerY, radius });
-}
-circles = shuffleArray(circles);
-let points = [];
-for (let i = 0; i < circles.length; i++) {
-  let pointsForCurrentCircle = [];
-  pointsForCurrentCircle = pointsOnCircle(circles[i], NB_POINTS_PER_CIRLCE);
-  pointsForCurrentCircle = shuffleArray(pointsForCurrentCircle);
-  points.push(...pointsForCurrentCircle);
-}
-
-// animate lines
+// animate
 let animationFrameId = null;
 let currentIndex = 0;
 let lastTimestamp = 0;
-function drawNextLine(timestamp) {
+let steps = 100;
+function drawNext(timestamp) {
   // Initialize lastTimestamp if first run
   if (!lastTimestamp) {
     lastTimestamp = timestamp;
@@ -80,27 +47,21 @@ function drawNextLine(timestamp) {
 
   // Only draw if enough time has passed
   if (elapsed >= DELAY_BETWEEN_LINES_MS) {
-    if (currentIndex >= NB_POINTS_PER_CIRLCE * circles.length) {
+    if (currentIndex >= steps) {
       stopAnimation();
       console.log("Animation complete!");
       return;
     }
 
-    drawLine(points[currentIndex], points[currentIndex + 1], canvasctx);
-    currentIndex += 2;
+    //drawLine(points[currentIndex], points[currentIndex + 1], canvasctx);
     lastTimestamp = timestamp;
   }
-  animationFrameId = requestAnimationFrame(drawNextLine);
+  animationFrameId = requestAnimationFrame(drawNext);
 }
 
 function startAnimation() {
-  if (showCircle) {
-    circles.forEach((circle) => {
-      drawCircle({ x: circle.x, y: circle.y }, circle.radius, canvasctx);
-    });
-  }
   if (!animationFrameId) {
-    animationFrameId = requestAnimationFrame(drawNextLine);
+    animationFrameId = requestAnimationFrame(drawNext);
   }
 }
 
